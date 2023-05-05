742d0cb43400c160d606650524ea60b298e0f01d
Om Prakash Mishra <opsgp1999@gmail.com>
2023-05-05 15:02:47 -0700

    ok

--- sparkel/src/App.js
+++ sparkel/src/App.js
@@ -1,9 +1,18 @@
-import './App.css';
+// import './App.css';
+
+import { SlInfo } from "react-icons/sl";
+import HomePage from "./Pages/HomePage";
+import SingleProductPage from "./Pages/SingleProductPage";
+import Navbar from "./Components/Navbar";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css"
+
 
 function App() {
   return (
     <div className="App">
-     
+      <Navbar/>
+  <SingleProductPage/>
     </div>
   );
 }
